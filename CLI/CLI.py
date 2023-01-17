a7b498d6111db6837b71706781cd7edc817ce5f0
Xeift <xeift.c@gmail.com>
2023-01-17 14:12:31 +0800

    add image support

--- CLI/CLI.py
+++ CLI/CLI.py
@@ -11,6 +11,12 @@ _embedColor = int(str(input('enter embed color (hex): ')), 16)
 _embedHyperlinkSetting = int(input('title hyperlink off/on, enter 1 or 2: '))
 announcementLog = []
 
+# I N P U T    E X A M P L E
+# _webhookURL = 'https://discord.com/api/webhooks/1064748597985423440/Iiyb5EeosAY7K1ZrPwIhdNWIeHNntPv6LBhcOtZ8RVIUTcNX3ujjvS95YydHVK3D4psH'
+# _tgAnnouncementChannel = 'dsafdsfa3243'
+# _embedColor = int(('0xe8006f'), 16)
+# _embedHyperlinkSetting = 2
+# announcementLog = []
 
 print('setup complete!')
 
@@ -25,7 +31,16 @@ while 1:
         for a in announcementBox:
             announcementLink = a.find_all('a',{'class':'tgme_widget_message_date'},href=True)[0]# get msg link
             announcementLink = announcementLink['href']
-            announcementText = a.find_all('div',{'class': 'tgme_widget_message_text js-message_text'})# get each msg
+            announcementText = a.find_all('div',{'class': 'tgme_widget_message_text js-message_text'})# get each text
+
+            announcementImg = a.find('a',{'class': 'tgme_widget_message_photo_wrap'},href=True)# get each img
+            if announcementImg != None:
+                startIndex = announcementImg['style'].find("background-image:url(\'") + 22
+                endIndex = announcementImg['style'].find(".jpg')") + 4
+                announcementImg = announcementImg['style'][startIndex:endIndex]
+            else:
+                announcementImg = None
+
             try:
                 announcementText = re.sub(# deal with linebreak
                     '<br/>',
@@ -37,13 +52,26 @@ while 1:
                 if announcementLink not in announcementLog:
                     announcementTemp.append(announcementLink)
                     webhook = SyncWebhook.from_url(_webhookURL)
-                    if _embedHyperlinkSetting == 1:
-                        embed = Embed(title='Forward From Telegram', description=announcementText, color=_embedColor)
-                    elif _embedHyperlinkSetting == 2:
-                        embed = Embed(title='Original Telegram Link', description=announcementText, url=f'{announcementLink}', color=_embedColor)
+                    
+                    if announcementText == None:
+                        if _embedHyperlinkSetting == 1:
+                            embed = Embed(title='Forward From Telegram', color=_embedColor)
+                        elif _embedHyperlinkSetting == 2:
+                            embed = Embed(title='Original Telegram Link', url=f'{announcementLink}', color=_embedColor)
+                    else:
+                        if _embedHyperlinkSetting == 1:
+                            embed = Embed(title='Forward From Telegram', description=announcementText, color=_embedColor)
+                        elif _embedHyperlinkSetting == 2:
+                            embed = Embed(title='Original Telegram Link', description=announcementText, url=f'{announcementLink}', color=_embedColor)
+                    
+                    if announcementImg != None:
+                        embed.set_image(url=announcementImg)
+
                     if announcementLog != []:
                         print(f'send {announcementLink}')
                         webhook.send(embed=embed)
+                else:
+                    print('y')
             except:
                 pass
 
